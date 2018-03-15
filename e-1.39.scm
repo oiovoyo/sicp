f3a3b2e4aef2a268ef0a7f2baa63a2f165e257a8
george <jqnk124@gmail.com>
2018-03-15 19:59:01 +0800

    词曰：     花褪残红青杏小     燕子飞时 绿水人家绕     枝上柳绵吹又少     天涯何处无芳草
        墙里秋千 墙外道
        墙外行人 墙里佳人笑
        笑渐不闻声渐消
        多情却被无情恼
    
        tan的定义都忘记了
        到底是sin/cos 还是cos/sin
        拿pi pi/2去试 是前者

---
+++ e-1.39.scm
@@ -0,0 +1,22 @@
+(load "e-1.37.scm")
+
+
+(define (D i)
+  (- (* 2 i ) 1)
+  )
+
+(define (tan-cf x k)
+  (define (N i)
+    (if (= i 1)
+       x 
+        (* x x -1)
+        )
+    )
+  (cont-frac-iter N D k 0.0)
+  )
+; tan pi is 0
+(tan-cf 3.14159265358978323846 10)
+; tan e+pi/2 is huge positive  
+(tan-cf 1.57089635358979323846 10)
+; tan -e+pi/s is huge negative
+(tan-cf 1.56989635358979323846 10)
