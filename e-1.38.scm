fc6fa40f44549a3936d4a6cc2ce6c023d9c6646d
george <jqnk124@gmail.com>
2018-03-15 19:33:40 +0800

    子曰：    学而不思则罔    思而不学则怠
      自然常数e 有多种求法
      本算法k取到20大约能精确到6位小数

---
+++ e-1.38.scm
@@ -0,0 +1,18 @@
+(load "e-1.37.scm")
+
+(define (N i) 1)
+
+(define (D i) 
+  ( if (= 0 (remainder (- i 2) 3))
+       (* 2 (/ (+ i 1) 3))
+       1
+       )
+  )
+
+(D 8)
+
+(define (E k)
+  (+ 2 (cont-frac-iter N D k 0.0))
+  )
+
+(E 20)
