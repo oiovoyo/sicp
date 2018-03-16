ea499fb965bcb18ba56d4d7da10a481a26c5f5f8
george <jqnk124@gmail.com>
2018-03-16 22:17:59 +0800

    诗云：     桃之夭夭     灼灼其华     之子于归     宜其室家
      几个题一直在考生成函数的函数，二阶函数
      得之于心应之于手

---
+++ e-1.43.scm
@@ -0,0 +1,22 @@
+(define (repeated f n)
+  (define (fn n x)
+    (if (= n 1)
+        (f x)
+        (f (fn (- n 1) x))
+        )
+    )
+  (lambda (x) (fn n x))
+  )
+
+((repeated square 2) 5)
+
+(load "compose.scm")
+
+(define (repeated-with-compose f n)
+  (if (= n 1)
+      f
+      (compose f (repeated-with-compose f (- n 1)))
+      )
+  )
+
+((repeated-with-compose square 2) 5)
